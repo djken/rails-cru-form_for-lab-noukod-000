efe9eecab90e69e5beff8399a4e9da0dcc014653
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-07-01 23:07:11 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,21 +1,21 @@
 class ArtistsController < ApplicationController
-    def index
-        @artists = Artist.all
-	end
+  def index
+    @artists = Artist.all
+  end
 
 	def show
 		@artist = Artist.find(params[:id])
 	end
 
 	def new
-		@artist = Artist.new
+    @artist = Artist.new
 	end
 
-    def create
-        @artist = Artist.new(artist_params)
-        @artist.save
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
         
-        redirect_to artist_path(@artist)
+    redirect_to artist_path(@artist)
 	end
 
 	def edit
@@ -31,6 +31,6 @@ class ArtistsController < ApplicationController
     private
     
     def artist_params
-         params.require(:artist).permit(:name, :bio)
+      params.require(:artist).permit(:name, :bio)
     end
 end
